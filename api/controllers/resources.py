b325fef246ca33e3fd70f64df1c1e4563f4e1fe8
Joseph Abdulwahab <jabdulwa@charlott.edu>
2024-07-14 05:42:07 -0400

    feat: implemented resources create function

---
+++ api/controllers/resources.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status, Response, Depends
+from ..models import models, schemas
+
+
+def create(db: Session, resource):
+    # Create a new instance of the Resource model with the provided data
+    db_resource = models.Resource(
+        item=resource.item,
+        amount=resource.amount
+    )
+    # Add the newly created Resource object to the database session
+    db.add(db_resource)
+    # Commit the changes to the database
+    db.commit()
+    # Refresh the Resource object to ensure it reflects the current state in the database
+    db.refresh(db_resource)
+    # Return the newly created Order object
+    return db_resource
+
+
